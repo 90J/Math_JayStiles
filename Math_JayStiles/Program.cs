413357bbc3c273749b0692acc9b86efb434c0580
Jay Stiles <JayStiles@quickenloans.com>
2018-10-12 16:00:33 -0400

    submitting

---
+++ Math_JayStiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_JayStiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //set variables
+            string userNum;
+            int conNumber;
+            int outputNumber;
+            int x = 0;
+
+            //get user input
+            Console.Write("Please, enter a number: ");
+            conNumber = Convert.ToInt32(Console.ReadLine());
+
+            //do math things and output data
+            while (x < 12)
+                {
+                x = x + 1;
+                outputNumber = conNumber * x;
+                Console.WriteLine(conNumber + "x" + x + "=" + outputNumber);
+
+            }
+        }
+    }
+}
